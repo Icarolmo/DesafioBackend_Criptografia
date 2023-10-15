c0248c400cd6bba830bedcc886e12ed4e73d6296
Icarus <icarolmol@gmail.com>
2023-10-15 18:19:19 -0300

    Refactor: changes the encode and descrypto sensitive data.

--- src/main/java/com/example/demoproject/controller/TransferController.java
+++ src/main/java/com/example/demoproject/controller/TransferController.java
@@ -13,8 +13,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
-import static com.example.demoproject.security.Cryptography.sha256;
-
 @RestController
 @RequestMapping("/transfer")
 public class TransferController {
@@ -23,24 +21,42 @@ public class TransferController {
     private TransferRepository repository;
 
     @Autowired
+    private SecurityComponent passwordEncoder;
+
+    @Autowired
     private EntityManager entityManager;
 
     @GetMapping
     public ResponseEntity getAllTransfer(){
         var allTransfer = repository.findAll();
+
+        allTransfer.forEach(transfer -> {
+            transfer.setUserDocument(passwordEncoder.decrypt(transfer.getUserDocument()));
+            transfer.setCreditCardToken(passwordEncoder.decrypt(transfer.getCreditCardToken()));
+        });
+
         return ResponseEntity.ok(allTransfer);
     }
 
     @GetMapping("/search")
     public ResponseEntity getAllTransferByFilter(@RequestParam(name = "id") String id) {
         var filteredTransfer = repository.findById(id);
+
+        filteredTransfer.ifPresent(transfer -> {
+            transfer.setUserDocument(passwordEncoder.decrypt(transfer.getUserDocument()));
+            transfer.setCreditCardToken(passwordEncoder.decrypt(transfer.getCreditCardToken()));
+        });
+
         return ResponseEntity.ok(filteredTransfer);
     }
 
     @PostMapping("/create")
     public ResponseEntity newTransfer(@RequestBody @Valid RequestTransfer data) throws Exception {
         Transfer newTransfer = new Transfer(data);
+        newTransfer.setUserDocument(passwordEncoder.encode(newTransfer.getUserDocument()));
+        newTransfer.setCreditCardToken(passwordEncoder.encode(newTransfer.getCreditCardToken()));
         Transfer responseTransfer = repository.save(newTransfer);
+        newTransfer.setId(responseTransfer.getId());
 
         URI uri = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -48,25 +64,33 @@ public class TransferController {
                 .buildAndExpand(responseTransfer.getId())
                 .toUri();
 
-        return ResponseEntity.created(uri).body(responseTransfer);
+        return ResponseEntity.created(uri).body(newTransfer);
     }
 
     @Transactional
     @PutMapping("/update")
     public ResponseEntity updateTransfer(@RequestParam(name = "id") String id, @RequestBody @Valid RequestTransfer data) throws Exception {
         Transfer transferAlter = repository.getReferenceById(id);
-        transferAlter.setUserDocument(sha256(data.userDocument()));
-        transferAlter.setCreditCardToken(sha256(data.creditCardToken()));
+
+        transferAlter.setUserDocument(passwordEncoder.encode(data.userDocument()));
+        transferAlter.setCreditCardToken(passwordEncoder.encode(data.creditCardToken()));
         transferAlter.setTransferValue(data.transferValue());
 
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("")
+                .buildAndExpand(transferAlter.getId())
+                .toUri();
+
         entityManager.merge(transferAlter);
-        return ResponseEntity.ok("Ok");
+        return ResponseEntity.created(uri).body(transferAlter);
     }
 
     @DeleteMapping("/delete")
     public ResponseEntity deleteTransfer(@RequestParam(name = "id") String id){
+        var transferDeleted = repository.findById(id);
         repository.deleteById(id);
-        return ResponseEntity.ok("Ok");
+        return ResponseEntity.ok().body(transferDeleted);
     }
 
 }
