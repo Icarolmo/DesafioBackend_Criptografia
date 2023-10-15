2990a5a1e189a2ff5e1a12879e6be7691b88a4b7
Icarus <icarolmol@gmail.com>
2023-10-15 00:48:31 -0300

    Refactor: Renamed files.

--- src/main/java/com/example/demoproject/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demoproject.repository;
-
-import com.example.demoproject.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
