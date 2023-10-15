ab4670b8170bbb591514b94b088754a095d41ba7
Icarus <icarolmol@gmail.com>
2023-10-15 00:48:15 -0300

    Refactor: Renamed from UserRepository for TransferRepository.

---
+++ src/main/java/com/example/demoproject/repository/TransferRepository.java
@@ -0,0 +1,7 @@
+package com.example.demoproject.repository;
+
+import com.example.demoproject.domain.Transfer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransferRepository extends JpaRepository<Transfer, String> {
+}
