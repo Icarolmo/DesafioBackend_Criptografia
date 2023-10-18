cb8a08b180ea93d0bd9764a841a7edfc3cb59973
Icarus <icarolmol@gmail.com>
2023-10-17 23:06:19 -0300

    Fix: Seated the secret fixed key for the error:secret descryption key different from the encryption key.

--- src/main/java/com/example/demoproject/controller/SecurityComponent.java
+++ src/main/java/com/example/demoproject/controller/SecurityComponent.java
@@ -1,41 +1,24 @@
 package com.example.demoproject.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
 import javax.crypto.spec.SecretKeySpec;
-import javax.crypto.SecretKey;
-import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 @Component
 public class SecurityComponent implements PasswordEncoder {
 
-    private static final byte[] SECRET_KEY;
-
-    static {
-        try {
-            SECRET_KEY = generateSecretKey();
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
-    public static byte[] generateSecretKey() throws NoSuchAlgorithmException {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-        keyGenerator.init(256);
-        SecretKey secretKey = keyGenerator.generateKey();
-        return secretKey.getEncoded();
-    }
+    @Value("${key.value}")
+    private String SECRET_KEY;
 
     @Override
     public String encode(CharSequence rawPassword) {
         try {
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            SecretKeySpec secretKey = new SecretKeySpec(SECRET_KEY, "AES");
+            SecretKeySpec secretKey = new SecretKeySpec(SECRET_KEY.getBytes(), "AES");
             cipher.init(Cipher.ENCRYPT_MODE, secretKey);
             byte[] encryptedTextBytes = cipher.doFinal(rawPassword.toString().getBytes("UTF-8"));
             return Base64.getEncoder().encodeToString(encryptedTextBytes);
@@ -52,7 +35,7 @@ public class SecurityComponent implements PasswordEncoder {
     public String decrypt(String encryptedPassword) {
         try {
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
-            SecretKeySpec secretKey = new SecretKeySpec(SECRET_KEY, "AES");
+            SecretKeySpec secretKey = new SecretKeySpec(SECRET_KEY.getBytes(), "AES");
             cipher.init(Cipher.DECRYPT_MODE, secretKey);
             byte[] decryptedTextBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedPassword));
             return new String(decryptedTextBytes);
