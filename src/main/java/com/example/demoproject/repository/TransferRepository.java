6dc965d802f81f16000380c0f984da22b4bc5d9f
Icarus <icarolmol@gmail.com>
2023-10-23 17:34:58 -0300

    Refactor: change type primary key

--- src/main/java/com/example/demoproject/repository/TransferRepository.java
+++ src/main/java/com/example/demoproject/repository/TransferRepository.java
@@ -3,5 +3,5 @@ package com.example.demoproject.repository;
 import com.example.demoproject.domain.Transfer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransferRepository extends JpaRepository<Transfer, String> {
+public interface TransferRepository extends JpaRepository<Transfer, Long> {
 }
