e86f3df46b7a91f053c1d99c1f0876e5e4086e6f
Icarus <icarolmol@gmail.com>
2023-10-23 17:34:21 -0300

    Refactor: create service package. change given responsibility of data persistence for service.

--- src/main/java/com/example/demoproject/controller/TransferController.java
+++ src/main/java/com/example/demoproject/controller/TransferController.java
@@ -1,15 +1,13 @@
 package com.example.demoproject.controller;
 
 import com.example.demoproject.domain.RequestTransfer;
-import com.example.demoproject.domain.Transfer;
-import com.example.demoproject.repository.TransferRepository;
-import jakarta.persistence.EntityManager;
+import com.example.demoproject.dto.TransferDTO;
+import com.example.demoproject.service.TransferService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
@@ -18,85 +16,64 @@ import java.net.URI;
 public class TransferController {
 
     @Autowired
-    private TransferRepository repository;
+    private TransferService service;
 
-    @Autowired
-    private SecurityComponent passwordEncoder;
-
-    @Autowired
-    private EntityManager entityManager;
-
-    @GetMapping
+    @GetMapping("/all")
     public ResponseEntity getAllTransfer(){
-        var allTransfer = repository.findAll();
-
-        allTransfer.forEach(transfer -> {
-            transfer.setUserDocument(passwordEncoder.decrypt(transfer.getUserDocument()));
-            transfer.setCreditCardToken(passwordEncoder.decrypt(transfer.getCreditCardToken()));
-        });
-
-        return ResponseEntity.ok(allTransfer);
+        try{
+            return ResponseEntity.ok().body(service.allTransfer());
+        } catch (RuntimeException e){
+            return ResponseEntity.internalServerError().body("Erro: " + e);
+        }
     }
 
     @GetMapping("/search")
-    public ResponseEntity getAllTransferByFilter(@RequestParam(name = "id") String id) {
-        var filteredTransfer = repository.findById(id);
-
-        filteredTransfer.ifPresent(transfer -> {
-            transfer.setUserDocument(passwordEncoder.decrypt(transfer.getUserDocument()));
-            transfer.setCreditCardToken(passwordEncoder.decrypt(transfer.getCreditCardToken()));
-        });
-
-        return ResponseEntity.ok(filteredTransfer);
+    public ResponseEntity getTranferById(@RequestParam(name = "id") String id) {
+        try{
+            TransferDTO response = service.transferById(Long.parseLong(id));
+            if(response != null){
+                return ResponseEntity.ok().body(response);
+            }
+            return ResponseEntity.notFound().build();
+        } catch (RuntimeException e){
+            return ResponseEntity.internalServerError().body("Erro: " + e);
+        }
     }
 
     @PostMapping("/create")
-    public ResponseEntity newTransfer(@RequestBody @Valid RequestTransfer data) throws Exception {
-
-        String encryptedUserDocument = passwordEncoder.encode(data.userDocument());
-        String encryptedCreditCardToken = passwordEncoder.encode(data.creditCardToken());
-
-        Transfer newTransfer = new Transfer(encryptedUserDocument, encryptedCreditCardToken, data.transferValue());
-
-        Transfer responseTransfer = repository.save(newTransfer);
-
-        responseTransfer.setUserDocument(data.userDocument());
-        responseTransfer.setCreditCardToken(data.creditCardToken());
-
-        URI uri = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("")
-                .buildAndExpand(responseTransfer.getId())
-                .toUri();
-
-        return ResponseEntity.created(uri).body(responseTransfer);
+    public ResponseEntity newTransfer(@RequestBody @Valid RequestTransfer data){
+        try{
+            TransferDTO responseBody = service.newTransfer(data);
+            URI uriCreate = service.createURI(responseBody.getId());
+            return ResponseEntity.created(uriCreate).body(responseBody);
+        } catch (RuntimeException e){
+            return ResponseEntity.internalServerError().body("Erro: " + e);
+        }
     }
 
     @Transactional
     @PutMapping("/update")
-    public ResponseEntity updateTransfer(@RequestParam(name = "id") String id, @RequestBody @Valid RequestTransfer data) throws Exception {
-        Transfer transferAlter = repository.getReferenceById(id);
-
-        transferAlter.setUserDocument(passwordEncoder.encode(data.userDocument()));
-        transferAlter.setCreditCardToken(passwordEncoder.encode(data.creditCardToken()));
-        transferAlter.setTransferValue(data.transferValue());
-
-        URI uri = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("")
-                .buildAndExpand(transferAlter.getId())
-                .toUri();
-
-        entityManager.merge(transferAlter);
-        return ResponseEntity.created(uri).body(transferAlter);
+    public ResponseEntity updateTransfer(@RequestParam(name = "id") String id, @RequestBody @Valid RequestTransfer data) {
+        try{
+            TransferDTO responseBody = service.updateTransfer(Long.parseLong(id), data);
+            return ResponseEntity.created(service.createURI(responseBody.getId())).body(responseBody);
+        } catch (RuntimeException e){
+            return ResponseEntity.internalServerError().body("Erro: " + e);
+        }
     }
 
     @Transactional
     @DeleteMapping("/delete")
     public ResponseEntity deleteTransfer(@RequestParam(name = "id") String id){
-        var transferDeleted = repository.findById(id);
-        repository.deleteById(id);
-        return ResponseEntity.ok().body(transferDeleted);
+        try{
+            TransferDTO responseBody = service.deleteTransfer(Long.parseLong(id));
+            if(responseBody == null){
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok().body(responseBody);
+        } catch (RuntimeException e){
+            return ResponseEntity.internalServerError().body("Erro: " + e);
+        }
     }
 
 }
